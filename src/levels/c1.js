8eb7d48f13555963cf106e43c6a40b2192564f5d
Jonathan Rebouças <jonathan@serel.com.br>
2020-09-27 18:22:50 -0300

    remoção vestidos vendidos

--- src/levels/c1.js
+++ src/levels/c1.js
@@ -172,66 +172,6 @@ const BuscarVestidos = (arrayMenssagens) => {
     nome: "Foto",
     descricao: "Valor *R$ 85,00*",
   });
-
-  arrayMenssagens.push({
-    destinatario: "cliente",
-    tipo: "imagem",
-    path: `${path.resolve(
-      __dirname,
-      "..",
-      "..",
-      "public",
-      "imagens",
-      "v9.jpeg"
-    )}`,
-    nome: "Foto",
-    descricao: "Valor *R$ 85,00*",
-  });
-
-  arrayMenssagens.push({
-    destinatario: "cliente",
-    tipo: "imagem",
-    path: `${path.resolve(
-      __dirname,
-      "..",
-      "..",
-      "public",
-      "imagens",
-      "v10.jpeg"
-    )}`,
-    nome: "Foto",
-    descricao: "Valor *R$ 85,00*",
-  });
-
-  arrayMenssagens.push({
-    destinatario: "cliente",
-    tipo: "imagem",
-    path: `${path.resolve(
-      __dirname,
-      "..",
-      "..",
-      "public",
-      "imagens",
-      "v11.jpeg"
-    )}`,
-    nome: "Foto",
-    descricao: "Valor *R$ 85,00*",
-  });
-
-  arrayMenssagens.push({
-    destinatario: "cliente",
-    tipo: "imagem",
-    path: `${path.resolve(
-      __dirname,
-      "..",
-      "..",
-      "public",
-      "imagens",
-      "v12.jpeg"
-    )}`,
-    nome: "Foto",
-    descricao: "Valor *R$ 85,00*",
-  });
 };
 
 const RespostaOpcaoInvalida = (arrayMenssagens) => {
