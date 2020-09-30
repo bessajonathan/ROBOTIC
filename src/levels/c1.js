edd0c1de331e73d977db85e0587f7bf25bf05581
Jonathan Rebouças <jonathan@serel.com.br>
2020-09-30 13:35:24 -0300

    vestido vendido

--- src/levels/c1.js
+++ src/levels/c1.js
@@ -82,21 +82,6 @@ const BuscarVestidos = (arrayMenssagens) => {
     nome: "Foto",
     descricao: "Valor *R$ 85,00*",
   });
-
-  arrayMenssagens.push({
-    destinatario: "cliente",
-    tipo: "imagem",
-    path: `${path.resolve(
-      __dirname,
-      "..",
-      "..",
-      "public",
-      "imagens",
-      "v3.jpeg"
-    )}`,
-    nome: "Foto",
-    descricao: "Valor *R$ 85,00*",
-  });
 };
 
 const RespostaOpcaoInvalida = (arrayMenssagens) => {
